ecf5965f0d0bcaaad572c4e1e407a509dc3c5b32
Julia Marushchenko <marushchenko.yulia@gmail.com>
2025-03-14 01:05:23 +0100

    Add files via upload
    Commiting class Main with of precedence rules

---
+++ src/com/operators/Main.java
@@ -0,0 +1,20 @@
+// Project with demonstration of precedence rules
+package com.operators;
+
+// Class Main
+public class Main {
+    public static void main(String[] args) {
+
+    //Local integer variables
+    int a = 3;
+    int b = 4;
+    int c = 5;
+    int d = 7;
+    int e = 2;
+    int f = 1;
+
+    // Printing solution to math equation
+    System.out.println("a + b / f - c + d * e = " + (a + b / f - c + d * e));
+
+    }
+}
